f22df711ff3833122765d41837f25a5f0593cd38
mdarbaz98 <mohdarbaz9969@gmail.com>
2022-05-18 18:28:51 +0530

    home half done

---
+++ js/custome.js
@@ -0,0 +1,2 @@
+$('.owl-carosel .owl-next span').html(`<i class="fa-solid fa-angle-right"></i>`);
+$('.owl-carosel .owl-prev span').html(`<i class="fa-solid fa-angle-left"></i>`);
---
+++ js/owlCarousel.js
@@ -0,0 +1,16 @@
+$('#home-owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1.3
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3.5
+        }
+    }
+})
