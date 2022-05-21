429d8a3a4c9ab81abd5cb999d6a8f11c16d552d2
mdarbaz98 <mohdarbaz9969@gmail.com>
2022-05-21 15:21:33 +0530

    product page complete

--- js/owlCarousel.js
+++ js/owlCarousel.js
@@ -31,3 +31,21 @@ $('#categoryproduct-owl-carousel').owlCarousel({
         }
     }
 })
+
+// product-owl-carousel
+$('#product-owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1.3
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3.5
+        }
+    }
+})
