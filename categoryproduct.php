f0da55f9354498e69e048cd92fd05b707dc94c0f
Nehashaikh913 <nehashaikh0500@gmail.com>
2022-05-30 10:02:44 +0530

    e23e

--- categoryproduct.php
+++ categoryproduct.php
@@ -4,19 +4,19 @@
             <div class="container">
                 <div class="row">
                     <div class="col-sm-12 col-md-12 col-lg-12 categorysection1_inside1">
-                        <img class="mt-5 pt-4" src="https://images.unsplash.com/photo-1532938911079-1b06ac7ceec7?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032"
+                        <img class="mt-5 pt-4" src="https://images.unsplash.com/photo-1581594693702-fbdc51b2763b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
                             alt="">
                     </div>
                     <div class="col-sm-12 col-md-12 col-lg-12 categorysection1_inside2">
                         <h1>Premature Ejaculation</h1>
                         <h2>All you need to know about Premature Ejaculation</h2>
                         <p>Premature Ejaculation means when you experience orgasm before the desired time. It is a common situation a man faces. Around 40 percent of males undergo this situation. 
-                            In Premature Ejaculation, a man can experience early orgasm before or between sexual intercourse, resulting in dissatisfaction with you and your partner. It can be depressing and stressful for a man, and there are several reasons that can raise PE, but firstly let’s normalize it for better understanding and your doubt clearance.
-                        <span>
-                            <br><a class="moreless-button" href="#">Read more...</a></span></p>
+                            In Premature Ejaculation, a man can experience early orgasm before or between sexual intercourse, resulting in dissatisfaction with you and your partner. It can be depressing and stressful for a man, and there are several reasons that can raise PE, but firstly let’s normalize it for better understanding and your doubt clearance.</p>
+                        <h2>How Ejaculation happen?</h2>
+                            <p>The ejaculation process occurs in a fast-action series of muscle contractions. When you reach the height of excitement during sex, the brain sends signals to your penis muscles, and they contract more to ejaculate the semen and it is also called the point of no return. Ejaculation process happens as a natural climax that is a sign of sexual satisfaction. In males semen ejaculates as a sign of sexual satisfaction, females get orgasm as a sign of their sexual satisafction. PE is a temporary state arising due to stress to perform during intercourse it can be due to mental pressure to satisfy the partner.
+                            <span><a class="moreless-button">Read more...</a></span>
+                            </p>
                         <div class="moretext" style="display: none;">
-                            <h2>How Ejaculation happen?</h2>
-                            <p>The ejaculation process occurs in a fast-action series of muscle contractions. When you reach the height of excitement during sex, the brain sends signals to your penis muscles, and they contract more to ejaculate the semen and it is also called the point of no return. Ejaculation process happens as a natural climax that is a sign of sexual satisfaction. In males semen ejaculates as a sign of sexual satisfaction, females get orgasm as a sign of their sexual satisafction. PE is a temporary state arising due to stress to perform during intercourse it can be due to mental pressure to satisfy the partner.</p>
                             <h2>Are There Any Symptoms Of Premature Ejaculation?</h2>
                             <p>Premature Ejaculation is not a disease that has symptoms. Early ejaculation is itself a sign of PE. If you ejaculate nearly in a minute every time you have sex or masturbation, then there are chances of Premature Ejaculation. You need to observe these instances at the time you are having sexual intercourse. </p>
                         </div>
--- js/owlCarousel.js
+++ js/owlCarousel.js
@@ -14,14 +14,3 @@ $('#home-owl-carousel,#product-owl-carousel,#categoryproduct-owl-carousel,#post-
         }
     }
 })
-
-
-// readmore
-$('.moreless-button').click(function() {
-    $('.moretext').slideToggle();
-    if ($('.moreless-button').text() == "Read more") {
-      $(this).text("Read less")
-    } else {
-      $(this).text("Read more")
-    }
-  });
