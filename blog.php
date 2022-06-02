110c6f6316bb2270fa8a8248fe00b06e302e0903
Fareenpeje <pejefareen1302@gmail.com>
2022-06-02 15:41:16 +0530

    aded

--- blog.php
+++ blog.php
@@ -2,29 +2,60 @@
       include('./include/header.php');
 ?>
     <section class="section_blogpage">
-        <div class="blog-heading-content">
+    <div class="blog-heading-content">
             <h1>PE blogs</h1>
             <p>Bring a difference in your everyday life with our blogs and articles. We provide you thorough and verified information so you can live the best way possible.</p>
         </div>
+    <?php
+            $stmt = $conn->prepare("SELECT * FROM `post` ORDER BY id DESC limit 6");
+            $stmt->execute();
+            $i=1;
+            $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            foreach ($data as $data)
+            {	
+                $stmt_img = $conn->prepare("SELECT * FROM `images` WHERE id=?");
+				$stmt_img->execute([$data['img_id']]);
+				$img_data = $stmt_img->fetchAll(PDO::FETCH_ASSOC);
+				if (!empty($img_data))
+                 {
+				    $image = $img_data[0]['path']; 
+					$alt = $img_data[0]['alt'];
+					}else{
+					$image="Not Found";
+					$alt="Not Found";
+					}								 
+	?>
+       
         <!-- 1 -->
         <div class="container blog-section">
             <div class="row">
                 <div class="col-sm-12 col-md-6 col-lg-6 blog-section-inside1">
                     <div class="blog-section_img">
-                        <img src="https://images.unsplash.com/photo-1484100356142-db6ab6244067?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHJhbmRvbXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500" alt="">
-                    </div>
+                    <img src="admin/<?php echo $image ?>" alt="<?php echo $alt  ?>" class="custome_img">                    </div>
                 </div>
                 <div class="col-sm-12 col-md-6 col-lg-6 blog-section-inside2">
-                  <span class="span1">Staying Healthy</span>
-                    <h1>Moving to wellness while practicing body neutrality</h1>
-                    <span class="span2">Published April 14, 2022</span>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio soluta excepturi praesentium perferendis assumenda ad dolores vitae! Ratione asperiores explicabo adipisci reprehenderit est totam, et ducimus quae distinctio quis perspiciatis!</p>
+                  <span class="span1">
+                      <?php
+															 $stmt_cat = $conn->prepare("SELECT * FROM `category` WHERE id=?");
+															 $stmt_cat->execute([$data['cat_id']]);
+															 $cat_data = $stmt_cat->fetchAll(PDO::FETCH_ASSOC);
+															 if (!empty($cat_data)) {
+																$cat_name = $cat_data[0]['name']; 
+																}else{
+																$cat_name="Not Found";
+															 }
+															 echo $cat_name;
+															 ?></span>
+                    <h1><?php echo $data['title']?></h1>
+                    <span class="span2">Published <?php echo $data['uploaded_on']?></span>
+                    <p><?php echo $data['description']?></p>
                     <button>Read more</button>
                 </div>
             </div>
         </div>
-        <!-- 2 --> 
-        <div class="container blog-section">
+        <?php $i++;} ?>
+        <!-- 2  -->
+        <!-- <div class="container blog-section">
             <div class="row">
                 <div class="col-sm-12 col-md-6 col-lg-6 blog-section-inside1">
                     <div class="blog-section_img">
@@ -39,9 +70,9 @@
                     <button>Read more</button>
                 </div>
             </div>
-        </div>
+        </div> -->
         <!-- 3 -->
-        <div class="container blog-section">
+        <!-- <div class="container blog-section">
             <div class="row">
                 <div class="col-sm-12 col-md-6 col-lg-6 blog-section-inside1">
                     <div class="blog-section_img">
@@ -56,9 +87,9 @@
                     <button>Read more</button>
                 </div>
             </div>
-        </div>
+        </div> -->
         <!-- 4 -->
-        <div class="container blog-section">
+        <!-- <div class="container blog-section">
             <div class="row">
                 <div class="col-sm-12 col-md-6 col-lg-6 blog-section-inside1">
                     <div class="blog-section_img">
@@ -73,9 +104,9 @@
                     <button>Read more</button>
                 </div>
             </div>
-        </div>
+        </div> -->
         <!-- 5 -->
-        <div class="container blog-section">
+        <!-- <div class="container blog-section">
             <div class="row">
                 <div class="col-sm-12 col-md-6 col-lg-6 blog-section-inside1">
                     <div class="blog-section_img">
@@ -90,9 +121,9 @@
                     <button>Read more</button>
                 </div>
             </div>
-        </div>
+        </div> -->
         <!-- 6 -->
-        <div class="container blog-section">
+        <!-- <div class="container blog-section">
             <div class="row">
                 <div class="col-sm-12 col-md-6 col-lg-6 blog-section-inside1">
                     <div class="blog-section_img">
@@ -107,7 +138,7 @@
                     <button>Read more</button>
                 </div>
             </div>
-        </div>
+        </div> -->
         <div class="outerline">
             <a href="">
             <button class="">View More</button>
