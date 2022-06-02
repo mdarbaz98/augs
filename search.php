0524bb0c7313c48591f2f85b2b38e184b1401830
Nehashaikh913 <nehashaikh0500@gmail.com>
2022-06-02 18:08:19 +0530

    search page

---
+++ search.php
@@ -0,0 +1,157 @@
+<?php include('./include/header.php') ?>
+        <section class="search_section">
+            <div class="container">
+                <div class="search_heading m-auto text-center">
+                <h1>Search:D</h1>
+                </div>
+            </div>
+            <div class="search_box_section">
+            <div class="container-fluid box_fluid">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                    <a href="">
+                        <div class="search_box">
+                            <span class="ribbon">Health psychology</span>
+                    <div class="seach_img">
+                        <img src="https://pasimage.gumlet.io/assets/upload/Trazodone-control-center.jpg?w=300&dpr=1.0" alt="">
+                    </div>
+                    <div class="search_content">
+                        <h2>Is Trazodone a controlled substance?</h2>
+                        <p>Trazodone is not a controlled substance nor a narcotic. Before consuming Trazodone, you should be cautious with its addictiveness.</p>
+                    </div>
+                    <div class="search_date">
+                        <p><span><i class="fa-solid fa-calendar-days"></i></span> 23 May 2022</p>
+                    </div>
+                    </div>
+                    </a>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                    <a href="">
+                        <div class="search_box">
+                            <span class="ribbon">Health psychology</span>
+                    <div class="seach_img">
+                        <img src="https://pasimage.gumlet.io/assets/upload/Trazodone-control-center.jpg?w=300&dpr=1.0" alt="">
+                    </div>
+                    <div class="search_content">
+                        <h2>Is Trazodone a controlled substance?</h2>
+                        <p>Trazodone is not a controlled substance nor a narcotic. Before consuming Trazodone, you should be cautious with its addictiveness.</p>
+                    </div>
+                    <div class="search_date">
+                        <p><span><i class="fa-solid fa-calendar-days"></i></span> 23 May 2022</p>
+                    </div>
+                    </div>
+                    </a>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                    <a href="">
+                        <div class="search_box">
+                            <span class="ribbon">Health psychology</span>
+                    <div class="seach_img">
+                        <img src="https://pasimage.gumlet.io/assets/upload/Trazodone-control-center.jpg?w=300&dpr=1.0" alt="">
+                    </div>
+                    <div class="search_content">
+                        <h2>Is Trazodone a controlled substance?</h2>
+                        <p>Trazodone is not a controlled substance nor a narcotic. Before consuming Trazodone, you should be cautious with its addictiveness.</p>
+                    </div>
+                    <div class="search_date">
+                        <p><span><i class="fa-solid fa-calendar-days"></i></span> 23 May 2022</p>
+                    </div>
+                    </div>
+                    </a>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                    <a href="">
+                        <div class="search_box">
+                            <span class="ribbon">Health psychology</span>
+                    <div class="seach_img">
+                        <img src="https://pasimage.gumlet.io/assets/upload/Trazodone-control-center.jpg?w=300&dpr=1.0" alt="">
+                    </div>
+                    <div class="search_content">
+                        <h2>Is Trazodone a controlled substance?</h2>
+                        <p>Trazodone is not a controlled substance nor a narcotic. Before consuming Trazodone, you should be cautious with its addictiveness.</p>
+                    </div>
+                    <div class="search_date">
+                        <p><span><i class="fa-solid fa-calendar-days"></i></span> 23 May 2022</p>
+                    </div>
+                    </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid box_fluid">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                    <a href="">
+                        <div class="search_box">
+                            <span class="ribbon">Health psychology</span>
+                    <div class="seach_img">
+                        <img src="https://pasimage.gumlet.io/assets/upload/Trazodone-control-center.jpg?w=300&dpr=1.0" alt="">
+                    </div>
+                    <div class="search_content">
+                        <h2>Is Trazodone a controlled substance?</h2>
+                        <p>Trazodone is not a controlled substance nor a narcotic. Before consuming Trazodone, you should be cautious with its addictiveness.</p>
+                    </div>
+                    <div class="search_date">
+                        <p><span><i class="fa-solid fa-calendar-days"></i></span> 23 May 2022</p>
+                    </div>
+                    </div>
+                    </a>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                    <a href="">
+                        <div class="search_box">
+                            <span class="ribbon">Health psychology</span>
+                    <div class="seach_img">
+                        <img src="https://pasimage.gumlet.io/assets/upload/Trazodone-control-center.jpg?w=300&dpr=1.0" alt="">
+                    </div>
+                    <div class="search_content">
+                        <h2>Is Trazodone a controlled substance?</h2>
+                        <p>Trazodone is not a controlled substance nor a narcotic. Before consuming Trazodone, you should be cautious with its addictiveness.</p>
+                    </div>
+                    <div class="search_date">
+                        <p><span><i class="fa-solid fa-calendar-days"></i></span> 23 May 2022</p>
+                    </div>
+                    </div>
+                    </a>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                    <a href="">
+                        <div class="search_box">
+                            <span class="ribbon">Health psychology</span>
+                    <div class="seach_img">
+                        <img src="https://pasimage.gumlet.io/assets/upload/Trazodone-control-center.jpg?w=300&dpr=1.0" alt="">
+                    </div>
+                    <div class="search_content">
+                        <h2>Is Trazodone a controlled substance?</h2>
+                        <p>Trazodone is not a controlled substance nor a narcotic. Before consuming Trazodone, you should be cautious with its addictiveness.</p>
+                    </div>
+                    <div class="search_date">
+                        <p><span><i class="fa-solid fa-calendar-days"></i></span> 23 May 2022</p>
+                    </div>
+                    </div>
+                    </a>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                    <a href="">
+                        <div class="search_box">
+                            <span class="ribbon">Health psychology</span>
+                    <div class="seach_img">
+                        <img src="https://pasimage.gumlet.io/assets/upload/Trazodone-control-center.jpg?w=300&dpr=1.0" alt="">
+                    </div>
+                    <div class="search_content">
+                        <h2>Is Trazodone a controlled substance?</h2>
+                        <p>Trazodone is not a controlled substance nor a narcotic. Before consuming Trazodone, you should be cautious with its addictiveness.</p>
+                    </div>
+                    <div class="search_date">
+                        <p><span><i class="fa-solid fa-calendar-days"></i></span> 23 May 2022</p>
+                    </div>
+                    </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+            </div>
+        
+        
+        </section>
+        <?php include('./include/footer.php') ?>
